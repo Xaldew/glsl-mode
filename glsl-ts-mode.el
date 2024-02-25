b4d5c4cc87a204d24a13a81bc412c2e3ba1e4dfc
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2024-02-25 23:47:10 +0100

    Most treesitter declarations now working.

--- glsl-ts-mode.el
+++ glsl-ts-mode.el
@@ -34,7 +34,7 @@
 
 
 (defvar glsl-ts-font-lock-rules
-  '(:language glsl
+  `(:language glsl
     :feature comment
     ((comment) @font-lock-comment-face)
 
@@ -58,7 +58,19 @@
                   (identifier) @font-lock-variable-name-face)
      (declaration (layout_specification "layout" @glsl-qualifier-face)
                   (extension_storage_class) @font-lock-keyword-face
-                  (identifier) @font-lock-variable-name-face))
+                  (identifier) @font-lock-variable-name-face)
+     (declaration type: (_) declarator: (identifier) @font-lock-variable-name-face)
+     (init_declarator declarator: (identifier) @font-lock-variable-name-face)
+     (parameter_declaration type: (_) declarator: (identifier) @font-lock-variable-name-face)
+     (parameter_declaration (["in" "out" "inout"] @font-lock-keyword-face)
+                            type: (_) declarator: (identifier) @font-lock-variable-name-face)
+     (field_declaration type: (_)
+                        declarator: [(field_identifier) @font-lock-variable-name-face
+                                     (array_declarator declarator: (field_identifier) @font-lock-variable-name-face)])
+     (call_expression function:
+                      ((identifier) @font-lock-type-face
+                       (:match ,(rx-to-string `(seq bol (or ,@glsl-type-list) eol)) @font-lock-type-face))))
+
 
     :language glsl
     :feature qualifier
